e1efddefd9e6bdf47f7e47e5f4cb3512fd53a6ba
shna rafeeq <shnar.majeed@gmail.com>
2020-06-13 18:07:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  
+  static titleize(string){
+    let words = string.split(' ');
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    words[0] = this.capitalize(words[0]);
+    for (let i = 1; i < words.length; i++) {
+      if(!except.find(e => e == words[i])){
+         words[i] = this.capitalize(words[i]);
+      }
+    }
+    
+    return words.join(' ');
+}
 }
